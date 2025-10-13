dfddd846b1416a5ab60cb838ab229272b895f76f
RoqueSPP <79621434+RoqueSPP@users.noreply.github.com>
2025-10-12 21:12:16 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 (function () {
-  const cep = document.querySelector("input[name=postalCode]");
+  const cep = document.querySelector("input[id=postalCode]");
 
   if (!cep) return; // evita erro se o campo não existir
 
@@ -25,10 +25,10 @@
         return;
       }
 
-      document.querySelector('input[name=address]')?.setAttribute('value', json.logradouro || '');
-      document.querySelector('input[name=neighborhood]')?.setAttribute('value', json.bairro || '');
-      document.querySelector('input[name=city]')?.setAttribute('value', json.localidade || '');
-      document.querySelector('input[name=state]')?.setAttribute('value', json.uf || '');
+      document.querySelector('input[id=address]')?.setAttribute('value', json.logradouro || '');
+      document.querySelector('input[id=neighborhood]')?.setAttribute('value', json.bairro || '');
+      document.querySelector('input[id=city]')?.setAttribute('value', json.localidade || '');
+      document.querySelector('input[id=state]')?.setAttribute('value', json.uf || '');
 
     } catch (err) {
       console.error("Erro ao consultar o CEP:", err);
